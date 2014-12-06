5521d50afbebf6955e084e015948e414124ed93e
t3kt <tommy.etkin@gmail.com>
2014-12-05 21:51:58 -0500

    fixing the simple renderer a bit
    apparently ofxbox2d decided to comment out chunks of the drawing code

--- src/SimpleRenderer.cpp
+++ src/SimpleRenderer.cpp
@@ -14,11 +14,19 @@ void SimpleRenderer::draw(RoundController &round) {
 }
 
 void SimpleRenderer::drawBrick(RoundController& round, Brick &brick) {
-  brick.rect().draw();
+  auto rect = brick.rect();
+  ofSetRectMode(OF_RECTMODE_CENTER);
+  ofSetColor(255, 0, 0);
+  ofRect(rect.body->GetPosition().x, rect.body->GetPosition().y,
+         rect.getWidth(), rect.getHeight());
 }
 
 void SimpleRenderer::drawPaddle(RoundController& round, Paddle &paddle) {
-  paddle.rect().draw();
+  auto rect = paddle.rect();
+  ofSetRectMode(OF_RECTMODE_CENTER);
+  ofSetColor(0, 255, 0);
+  ofRect(rect.body->GetPosition().x, rect.body->GetPosition().y,
+         rect.getWidth(), rect.getHeight());
 }
 
 void SimpleRenderer::drawBall(RoundController& round, Ball &ball) {
