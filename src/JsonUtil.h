2e5b8f6a8b192f40b73c5e5d18d972a0426bb6ba
t3kt <tommy.etkin@gmail.com>
2014-12-11 23:17:38 -0500

    adding more json serialization stuff (see #5)

---
+++ src/JsonUtil.h
@@ -0,0 +1,45 @@
+//
+//  JsonUtil.h
+//  bleepout
+//
+//  Created by tekt on 12/11/14.
+//
+//
+
+#ifndef __bleepout__JsonUtil__
+#define __bleepout__JsonUtil__
+
+#include <json.h> // it's included as part of ofxLibwebsockets
+#include <ofMain.h>
+#include <vector>
+#include "BleepoutConfig.h"
+
+bool readJsonVal(const Json::Value& obj, const char* property, float* result);
+bool readJsonVal(const Json::Value& obj, const char* property, int* result);
+bool readJsonVal(const Json::Value& obj, const char* property, unsigned char* result);
+bool readJsonVal(const Json::Value& obj, const char* property, bool* result);
+bool readJsonVal(const Json::Value& obj, const char* property, ofVec2f* result);
+bool readJsonVal(const Json::Value& obj, const char* property, ofVec3f* result);
+bool readJsonVal(const Json::Value& obj, const char* property, PhysicsOptions* result);
+bool readJsonVal(const Json::Value& obj, const char* property, ofColor* result);
+bool readJsonVal(const Json::Value& obj, const char* property, std::vector<ofColor>* result);
+
+template<typename TEnum>
+bool readJsonEnumVal(const Json::Value& obj, const char* property, TEnum* result) {
+  int valueTemp;
+  if(!readJsonVal(obj, property, &valueTemp))
+    return false;
+  *result = (TEnum)valueTemp;
+  return true;
+}
+
+Json::Value toJsonObj(const ofVec2f& val);
+Json::Value toJsonObj(const ofVec3f& val);
+Json::Value toJsonObj(const PhysicsOptions& val);
+Json::Value toJsonObj(const ofColor& val);
+Json::Value toJsonArr(const std::vector<ofColor>& vals);
+
+bool readJsonObjectFile(std::string path, Json::Value* obj);
+void writeJsonFile(std::string path, const Json::Value& obj);
+
+#endif /* defined(__bleepout__JsonUtil__) */
