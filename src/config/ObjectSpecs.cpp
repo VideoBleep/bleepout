a30facba12316cd8513d65005d2b456daa34e4ea
t3kt <tommy.etkin@gmail.com>
2014-12-27 22:17:35 -0500

    added more brick configurations

--- src/config/ObjectSpecs.cpp
+++ src/config/ObjectSpecs.cpp
@@ -7,3 +7,41 @@
 //
 
 #include "ObjectSpecs.h"
+#include "BleepoutConfig.h"
+
+void BrickQuadClusterRingsSpec::addBricksTo(RoundConfig *config) const {
+  config->addBrickRing()
+    .setElevation(elevation - elevationSpacing)
+    .setCount(count)
+    .setColor(color1)
+    .setPhase(-headingSpacing)
+    .setSize(size)
+    .setSpeed(speed)
+    .setModifier(modifierName, modifierChance)
+    .setValue(2).setLives(2);
+  config->addBrickRing()
+    .setElevation(elevation - elevationSpacing)
+    .setCount(count)
+    .setColor(color2)
+    .setSize(size)
+    .setSpeed(speed)
+    .setModifier(modifierName, modifierChance)
+    .setPhase(headingSpacing);
+  config->addBrickRing()
+    .setElevation(elevation + elevationSpacing)
+    .setCount(count)
+    .setColor(color2)
+    .setSize(size)
+    .setSpeed(speed)
+    .setModifier(modifierName, modifierChance)
+    .setPhase(-headingSpacing);
+  config->addBrickRing()
+    .setElevation(elevation + elevationSpacing)
+    .setCount(count)
+    .setColor(color1)
+    .setPhase(headingSpacing)
+    .setSize(size)
+    .setSpeed(speed)
+    .setModifier(modifierName, modifierChance)
+    .setValue(2).setLives(2);
+}
