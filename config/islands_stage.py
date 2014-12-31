ce938d720d34b2a398502565709905f51eeaf1c5
t3kt <tommy.etkin@gmail.com>
2014-12-31 13:33:41 -0500

    minor tweaks to the islands stage

--- config/islands_stage.py
+++ config/islands_stage.py
@@ -77,55 +77,6 @@ def generate():
                      modifier={"name": widePaddle["name"], "chance": 0.1})
   ]
 
-  # objects += [
-  #   createBrickRing(elevation=68,
-  #                   color=[0, 0, 255],
-  #                   count=24,
-  #                   value=2,
-  #                   lives=2,
-  #                   size=[7, 5, 10],
-  #                   modifier={"name": superCharge["name"], "chance": 0.2}),
-  #   createBrickRing(elevation=64,
-  #                   color=[0, 63, 255],
-  #                   count=48,
-  #                   value=2,
-  #                   lives=2,
-  #                   size=[7, 5, 8],
-  #                   modifier={"name": narrowPaddle["name"], "chance": 0.4}),
-  #   createBrickRing(elevation=56,
-  #                   color=[0, 127, 255],
-  #                   count=36,
-  #                   value=1,
-  #                   lives=1,
-  #                   size=[7, 5, 12],
-  #                   modifier={"name": widePaddle["name"], "chance": 0.1}),
-  #   createBrickRing(elevation=48,
-  #                   color=[0, 192, 127],
-  #                   count=48,
-  #                   value=2,
-  #                   lives=2,
-  #                   size=[7, 5, 14],
-  #                   speed=0.02),
-  #   createBrickRing(elevation=44,
-  #                   color=[0, 127, 192],
-  #                   count=56,
-  #                   value=1,
-  #                   lives=1,
-  #                   size=[7, 5, 12]),
-  #   createBrickRing(elevation=40,
-  #                   color=[0, 127, 192],
-  #                   count=56,
-  #                   value=1,
-  #                   lives=1,
-  #                   size=[7, 5, 12]),
-  #   createBrickRing(elevation=36,
-  #                   color=[0, 127, 192],
-  #                   count=56,
-  #                   value=1,
-  #                   lives=1,
-  #                   size=[7, 5, 12])
-  # ]
-
   # create the floor exit wall
   objects.append(createFloor(True))
 
@@ -144,19 +95,12 @@ def generate():
                   radiusScale=1.4,
                   lineWidth=2.4,
                   count=20,
-                  color=[255, 0, 127, 127]),
-    # createRingSet(spin=createValuePulser(0, 0.2, 5, [0, 30, 30]),
-    #               spread=createValuePulser(0.01, 0.16, 10, [0, 50, 0]),
-    #               spreadOffset=[60, 60, 90],
-    #               count=150,
-    #               radiusScale=4,
-    #               lineWidth=.9,
-    #               color=[0, 255, 63, 176])
+                  color=[255, 0, 127, 127])
   ]
   # messages
   objects += [
     createMessage(text="STAGE # START",
-                  color=[0, 127, 127],
+                  color=[255, 0, 127],
                   size=25,
                   delay=1,
                   duration=3)
